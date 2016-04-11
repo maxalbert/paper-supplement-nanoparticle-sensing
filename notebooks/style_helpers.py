2ba9eaba0bcb229055db09147f1cb654190badbf
Maximilian Albert <maximilian.albert@gmail.com>
2016-04-11 22:23:20 +0000

    Use a list for the style cycle so that subsequent calls to the plotting functions don't mix up the line styles.

--- notebooks/style_helpers.py
+++ notebooks/style_helpers.py
@@ -1,13 +1,13 @@
 import brewer2mpl
-import itertools
 from cycler import cycler
 
+N = 5
 
-cmap = brewer2mpl.get_map('Set1', 'Qualitative', 5, reverse=False)
+cmap = brewer2mpl.get_map('Set1', 'Qualitative', N, reverse=False)
 color_cycle = cycler('color', cmap.hex_colors)
 marker_cycle = cycler('marker', ['s', '^', 'o', 'D', 'v'])
 markersize_cycle = cycler('markersize', [10, 12, 11, 10, 12])
-style_cycle = itertools.cycle(color_cycle + marker_cycle + markersize_cycle)
+style_cycle = list(color_cycle + marker_cycle + markersize_cycle)[:N]
 
 cmap = brewer2mpl.get_map('Set1', 'Qualitative', 3, reverse=False)
 color_cycle = cycler('color', ['black', '#88CCDD', '#c73027'])
@@ -16,4 +16,4 @@ markersize_cycle = cycler('markersize', [8, 8, 8])
 fillstyle_cycle = cycler('fillstyle', ['full', 'full', 'full'])
 linestyle_cycle = cycler('linestyle', ['dashed', 'solid', 'solid'])
 linewidth_cycle = cycler('linewidth', [2, 2.25, 2])
-style_cycle_fig7 = (color_cycle + marker_cycle + markersize_cycle + fillstyle_cycle + linestyle_cycle + linewidth_cycle)
+style_cycle_fig7 = list(color_cycle + marker_cycle + markersize_cycle + fillstyle_cycle + linestyle_cycle + linewidth_cycle)[:N]
